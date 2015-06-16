d0496a66f6281bf8a3f71338637795dc13a034f7
Michael Z <michael2012z@gmail.com>
2015-06-16 17:13:46 +0800

    update

--- bookMaker.py
+++ bookMaker.py
@@ -9,6 +9,8 @@ import xml.dom.minidom
 from decimal import Decimal
 from re import sub
 
+import commands
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -20,7 +22,7 @@ bookList = [
     ['孟子', [['', ['梁惠王上', '梁惠王下', '公孫丑上', '公孫丑下', '滕文公上', '滕文公下', '離婁上', '離婁下', '萬章上', '萬章下', '告子上', '告子下', '盡心上', '盡心下']]]],
     ['論語', [['', ['學而', '為政', '八佾', '里仁', '公冶長', '雍也', '述而', '泰伯', '子罕', '鄉黨', '先進', '顏淵', '子路', '憲問', '衛靈公', '季氏', '陽貨', '微子', '子張', '堯曰']]]],
     ['禮記', [['', ['曲禮上', '曲禮下', '檀弓上', '檀弓下', '王制', '月令', '曾子問', '文王世子', '禮運', '禮器', '郊特牲', '內則', '玉藻', '明堂位', '喪服小記', '大傳', '少儀', '學記', '樂記', '雜記上', '雜記下', '喪大記', '祭法', '祭義', '祭統', '經解', '哀公問', '仲尼燕居', '孔子閒居', '坊記', '中庸', '表記', '緇衣', '奔喪', '問喪', '服問', '間傳', '三年問', '深衣', '投壺', '儒行', '大學', '冠義', '昏義', '鄉飲酒義', '射義', '燕義', '聘義', '喪服四制']]]],
-    ['莊子', [['內篇', ['逍遙遊', '相關討論', '', '齊物論', '養生主', '人間世', '德充符', '大宗師', '應帝王', '相關討論']], ['外篇', ['駢拇', '馬蹄', '胠篋', '在宥', '天地', '天道', '天運', '刻意', '繕性', '秋水', '至樂', '達生', '山木', '田子方', '知北遊']], ['雜篇', ['庚桑楚', '徐無鬼', '則陽', '外物', '寓言', '讓王', '盜跖', '說劍', '漁父', '列御寇', '天下']]]],
+    ['莊子', [['內篇', ['逍遙遊', '齊物論', '養生主', '人間世', '德充符', '大宗師', '應帝王']], ['外篇', ['駢拇', '馬蹄', '胠篋', '在宥', '天地', '天道', '天運', '刻意', '繕性', '秋水', '至樂', '達生', '山木', '田子方', '知北遊']], ['雜篇', ['庚桑楚', '徐無鬼', '則陽', '外物', '寓言', '讓王', '盜跖', '說劍', '漁父', '列御寇', '天下']]]],
     ['四書章句集注', [['大學章句', ['大學章句序', '大學章句']], ['中庸章句', ['中庸章句序', '中庸章句']], ['論語集注', ['論語序說', '讀論語孟子法', '學而第一', '為政第二', '八佾第三', '里仁第四', '公冶長第五', '雍也第六', '述而第七', '泰伯第八', '子罕第九', '鄉黨第十', '先進第十一', '顏淵第十二', '子路第十三', '憲問第十四', '衛靈公第十五', '季氏第十六', '陽貨第十七', '微子第十八', '子張第十九', '堯曰第二十']], ['孟子集注', ['孟子序說', '梁惠王章句上', '梁惠王章句下', '公孫丑章句上', '公孫丑章句下', '滕文公章句上', '滕文公章句下', '離婁章句上', '離婁章句下', '萬章章句上', '萬章章句下', '告子章句上', '告子章句下', '盡心章句上', '盡心章句下']]]]
     ]
 
@@ -29,14 +31,14 @@ class WebPageParser(HTMLParser):
     textOn = False
     text = []
     textPiece = ""
-    cClass = "content"
+    cClass = "normal"
 
     def __init__(self):
         HTMLParser.__init__(self)
         self.text = []
         self.textOn = False
         self.textPiece = ""
-        self.cClass = "content"
+        self.cClass = "normal"
 
     def parse(self, html):
         try:
@@ -53,7 +55,10 @@ class WebPageParser(HTMLParser):
         if tag == "td" and len(attrs) == 1 and attrs[0] == ('class', 'ctext'):
             self.textOn = True
             self.textPiece = ""
-            self.cClass = "content"
+            self.cClass = "normal"
+        elif self.textOn == True and tag == "span" and len(attrs) == 1 and attrs[0] == ('class', 'original'):
+            self.textPiece = ""
+            self.cClass = "original"
 
     def handle_data(self, data):
         if self.textOn == True:
@@ -64,6 +69,11 @@ class WebPageParser(HTMLParser):
         if tag == "td" and self.textOn == True:
             self.text.append([self.cClass, self.textPiece])
             self.textOn = False
+            self.textPiece = ""
+        elif self.textOn == True and tag == "span":
+            self.text.append([self.cClass, self.textPiece])
+            self.cClass = "normal"
+            self.textPiece = ""
 
 htmlText1 = '''
 <?xml version="1.0" encoding="utf-8"?>
@@ -142,13 +152,16 @@ if __name__ == '__main__':
     for book in bookList:
         bookName = book[0]
         groupList = book[1]
+        i = 0
         for group in groupList:
             groupName = group[0]
             chapterList = group[1]
-            i = 0
             for chapter in chapterList:
                 i += 1
-                fileName = '' + bookName + '/raw/' + bookName + ' : ' + chapter + ' - 中國哲學書電子化計劃.html'
+                if groupName == '':
+                    fileName = '' + bookName + '/raw/' + bookName + ' : ' + chapter + ' - 中國哲學書電子化計劃.html'
+                else:
+                    fileName = '' + bookName + '/raw/' + bookName + ' : ' + groupName + ' : ' + chapter + ' - 中國哲學書電子化計劃.html'
                 print fileName
                 f = open(fileName, "r")
                 rawText = f.read()
@@ -163,7 +176,9 @@ if __name__ == '__main__':
                 htmlText += '<div id="ch' + str(i) + '"></div>\n'
                 htmlText += '<h2>' + chapter + '</h2>\n'
                 for textLine in textLines:
-                    htmlText += '<p class=\"' + textLine[0] + '\">' + textLine[1] + '</p>\n'
+                    htmlText += '<span class=\"' + textLine[0] + '\">' + textLine[1] + '</span><br/>\n'
+                    if textLine[0] == 'normal':
+                        htmlText += '<p/>\n'
                 htmlText += htmlText3
                 fileName = bookName + "/" + bookName + "_" + chapter + ".html"
                 f = open(fileName, 'w')
@@ -174,18 +189,20 @@ if __name__ == '__main__':
         htmlText = htmlText1
         htmlText += 'Table Of Contents'
         htmlText += htmlText2
-        htmlText += '<h2 id="toc">Table Of Contents</h2>\n'
-        htmlText += '<ul>\n'
+        htmlText += '<h2 id="toc">目錄</h2>\n'
         groupList = book[1]
+        i = 0
         for group in groupList:
             groupName = group[0]
+            if groupName <> '':
+                htmlText += groupName + '<br/>'
             chapterList = group[1]
-            i = 0
+            htmlText += '<ul>\n'
             for chapter in chapterList:
                 i += 1
                 fileName = bookName + "_" + chapter + ".html"
                 htmlText += '<li><a href="' + fileName + '#ch' + str(i) + '">' + chapter + '</a></li>\n'
-        htmlText += '</ul>\n'
+            htmlText += '</ul>\n'
         htmlText += htmlText3
         f = open(bookName + "/toc.html", 'w')
         f.write(htmlText)
@@ -205,10 +222,10 @@ if __name__ == '__main__':
 			<content src="toc.html#toc" />
 		</navPoint>'''
         groupList = book[1]
+        i = 0
         for group in groupList:
             groupName = group[0]
             chapterList = group[1]
-            i = 0
             for chapter in chapterList:
                 i += 1
                 fileName = bookName + "_" + chapter + ".html"
@@ -241,19 +258,19 @@ if __name__ == '__main__':
         opfText += bookName
         opfText += opfText2
         groupList = book[1]
+        i = 0
         for group in groupList:
             groupName = group[0]
             chapterList = group[1]
-            i = 0
             for chapter in chapterList:
                 i += 1
                 opfText += '<item id="ch' + str(i) + '" media-type="text/x-oeb1-document" href="' + bookName + '_' + chapter + '.html" />\n'
         opfText += opfText3
         groupList = book[1]
+        i = 0
         for group in groupList:
             groupName = group[0]
             chapterList = group[1]
-            i = 0
             for chapter in chapterList:
                 i += 1
                 opfText += '<itemref idref="ch' + str(i) + '"/>\n'
@@ -264,3 +281,14 @@ if __name__ == '__main__':
         f.write(opfText)
         f.close()
 
+        # style.css
+        styleText = '.pagebreak { page-break-before: always; }\n.original {font-weight: bold; background-color: #EEE;}'
+        f = open(bookName + "/" + "style.css", 'w')
+        f.write(styleText)
+        f.close()
+
+        # generate
+        commandText = 'cd ' + bookName + '; ' + '../kindlegen/kindlegen ' + bookName + '.opf' + '; cp ' + bookName + '.mobi ' + '../output/' 
+        (status, output) = commands.getstatusoutput(commandText)
+        print commandText
+        print output
